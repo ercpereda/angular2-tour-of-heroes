c4bd67f4ae50d19abc49bfcdd4a0fe2dc5ab2399
E. R. C. Pereda <ercp42@gmail.com>
2016-06-26 14:02:56 -0300

    show and edit hero data with one-way and two-way data bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 import '../../public/css/styles.css';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
   template: require('./app.component.html'),
   styles: [require('./app.component.css')]
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
